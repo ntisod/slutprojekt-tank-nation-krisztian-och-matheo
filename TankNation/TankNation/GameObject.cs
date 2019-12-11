e549ba2478d6e94a50a50a668efd03e8c156680c
Kriszz986 <43604960+Kriszz986@users.noreply.github.com>
2019-12-11 10:28:49 +0100

    Yeah
    Yeah

--- TankNation/TankNation/GameObject.cs
+++ TankNation/TankNation/GameObject.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace TankNation
 {
     class GameObject
     {
+        protected Texture2D gfx;
+        protected Vector2 pos;
+
+        public GameObject(Texture2D gfx, float x, float y)
+        {
+            this.gfx = gfx;
+        }
     }
 }
